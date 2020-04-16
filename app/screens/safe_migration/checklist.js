c8eee1b0a9847dcd4d8ae3e4e1055556967847e8
Sokly <hengsokly23@gmail.com>
2020-04-16 08:41:31 +0700

    add check list screen

--- app/screens/safe_migration/checklist.js
+++ app/screens/safe_migration/checklist.js
@@ -18,11 +18,36 @@ import uuidv4 from '../../utils/uuidv4';
 export default class Checklist extends React.Component {
   state = {};
 
+  _renderList() {
+    let list = [
+      'កាតពលករកម្ពុជាធ្វើការក្រៅប្រទេស',
+      'លិខិតឆ្លងដែន',
+      'ទិដ្ឋាការការងារ',
+      'កិច្ចសន្យារការងារ',
+      'កិច្ចសន្យាររកការងារឱ្យធ្វើ',
+      'លិខិតអនុញ្ញាតឱ្យស្នាក់នៅក្នុងប្រទេសទទួល',
+      'ឯកសារផ្សេងៗ'
+    ];
+
+    return list.map(value => (
+      <View key={uuidv4()} style={{flexDirection: 'row', alignItems: 'center', marginBottom: 8}}>
+        <Icon name='radio-button-checked' size={24}/>
+        <Text style={{marginLeft: 10}}>{value}</Text>
+      </View>
+    ));
+  }
+
   render() {
     return (
       <ScrollView style={{flex: 1}}>
         <View style={Style.container}>
-          <Text>Checklist</Text>
+          <View style={Style.card}>
+            <Text style={{fontFamily: FontFamily.title, marginBottom: 10}}>ឯកសារដែលអ្នកត្រូវមានដូចជា៖</Text>
+
+            <View>
+              { this._renderList() }
+            </View>
+          </View>
         </View>
       </ScrollView>
     );
