79829d0a62b8a44a20468542f3078dfef121c11d
Sovanda <sovanda.chhou@gmail.com>
2021-01-06 16:03:26 +0700

    Splash screen

--- app/assets/stylesheets/base/font.js
+++ app/assets/stylesheets/base/font.js
@@ -1,6 +1,6 @@
 export const FontFamily = {
-  body: 'KhmerOSContentRegular',
-  title: 'KhmerOSContentBold'
+  body: 'KhmerOSContent',
+  title: 'KhmerOSContent-Bold'
 };
 
 export const FontSize = {
