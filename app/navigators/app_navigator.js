e28068d789e86bc04e8a0fd2a9f508d491d53099
Sovanda <sovanda.chhou@gmail.com>
2021-01-27 17:00:47 +0700

    Create your story screen (Test screen)

--- app/navigators/app_navigator.js
+++ app/navigators/app_navigator.js
@@ -39,6 +39,7 @@ import YourSafetyScreen from '../screens/your_safety/your_safety';
 import YourRightsAndSafetyScreen from '../screens/your_safety/your_rights_and_safety';
 import SafetyPlanningScreen from '../screens/your_safety/safety_planning';
 import YourSafetyVideosScreen from '../screens/your_safety/videos';
+import YourStoryScreen from '../screens/your_story/your_story';
 import CreateYourStoryScreen from '../screens/your_story/create_your_story';
 
 const Stack = createStackNavigator();
@@ -128,6 +129,8 @@ export default class AppNavigator extends Component {
           <Stack.Screen name="SafetyPlanningScreen" component={SafetyPlanningScreen} options={{ headerShown: false }} />
 
           <Stack.Screen name="YourSafetyVideosScreen" component={YourSafetyVideosScreen} options={{ headerShown: false }} />
+
+          <Stack.Screen name="YourStoryScreen" component={YourStoryScreen} options={{ headerShown: false }} />
 
           <Stack.Screen name="CreateYourStoryScreen" component={CreateYourStoryScreen} options={{ headerShown: false }} />
 
