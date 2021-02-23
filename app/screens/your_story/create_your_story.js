39bc29659b7f4448f006bea6d7494e4f2ef8a5f7
Sovanda <sovanda.chhou@gmail.com>
2021-02-23 16:02:41 +0700

    improvement create your story skip logic

--- app/screens/your_story/create_your_story.js
+++ app/screens/your_story/create_your_story.js
@@ -64,6 +64,7 @@ export default class CreateYourStory extends React.Component {
 
     this.state = {
       current_question_index: 0,
+      answer: [],
       answer_id: 1,
       progress: 0,
       question_length: get_questionnaires_data.length,
@@ -79,14 +80,44 @@ export default class CreateYourStory extends React.Component {
     return progress_percentage;
   }
 
+  _onSelectAnswer(item) {
+    let isSelected = this.state.answer.find(i => {
+      return i.id == item.id;
+    })
+
+    if (isSelected) {
+      let filter_array = this.state.answer.filter(item => {
+        return item.id !== isSelected.id;
+      });
+
+      this.setState({
+        answer: filter_array
+      })
+
+    } else {
+      let answer = [...this.state.answer];
+
+      answer.push({
+        id: item.id,
+        weight: item.weight
+      });
+
+      this.setState({ answer });
+
+    }
+
+  }
+
   _renderCard(item) {
-    let isSelected = this.state.answer_id == item.id;
+
+    let isSelected = this.state.answer.find(i => i.id == item.id)
     let selectedAnswer = isSelected ? { backgroundColor: Color.pink } : { backgroundColor: Color.gray };
 
     return (
       <TouchableOpacity
         key={uuidv4()}
-        onPress={() => this.setState({ answer_id: item.id })}
+        // onPress={() => this.setState({ answer_id: item.id })}
+        onPress={() => this._onSelectAnswer(item)}
         style={[Style.card, styles.answerCard]}
         activeOpacity={0.8}
       >
@@ -160,6 +191,7 @@ export default class CreateYourStory extends React.Component {
   _checkSelectedAnswer() {
     let current_question = this._get_current_question();
     let answer_id = this.state.answer_id;
+    let answer = this.state.answer;
     let selected_answer = {};
 
     let selected_answer_weight = current_question.options.find(item => {
@@ -167,11 +199,134 @@ export default class CreateYourStory extends React.Component {
     })
 
     selected_answer = {
+      id: selected_answer_weight.id,
       code: current_question.code,
-      weight: selected_answer_weight.weight
+      answers: answer,
     }
 
     return selected_answer;
+  }
+
+  _checkNextQuestionV2() {
+    let current_question_index = this.state.current_question_index;
+    let all_questions = this.state.questionnaires;
+
+    let update_state = {
+      answers: [...this.state.answers, {
+        order: all_questions[current_question_index].order,
+        code: all_questions[current_question_index].code,
+        answers: [...this.state.answer]
+      }]
+    };
+
+    let next_question_index = -1;
+
+    for (let i = current_question_index + 1; i < all_questions.length; i++) {
+      const question = all_questions[i];
+
+      if (question.skip_logic !== null) {
+        let operator = question.skip_logic.operator;
+
+        if (operator == 'and') {
+          let skip_flag = true;
+
+          for (let j = 0; j < question.skip_logic.criterias.length; j++) {
+            let criteria = question.skip_logic.criterias[j];
+
+            var get_answer = update_state.answers.find(obj => {
+              return obj.code == criteria.code;
+            })
+
+            if (!get_answer) {
+              skip_flag = false;
+              break;
+            }
+
+            if (criteria.operator == 'match_any') {
+
+              const match_any = criteria.value.some(e => get_answer.answers.find(k => k.id == e) ? true : false);
+              skip_flag &= match_any;
+
+            } else if (criteria.operator == 'match_all') {
+
+              const match_all = criteria.value.every(e => get_answer.answers.find(k => k.id == e) ? true : false);
+              skip_flag &= match_all;
+
+            }
+
+          }
+
+          if (skip_flag) {
+            next_question_index = i;
+          }
+
+        } else if (operator == 'or') {
+          let skip_flag = false;
+
+          for (let j = 0; j < question.skip_logic.criterias.length; j++) {
+            let criteria = question.skip_logic.criterias[j];
+
+            var get_answer = update_state.answers.find(obj => {
+              return obj.code == criteria.code;
+            })
+
+            if (get_answer) {
+              if (criteria.operator == 'match_any') {
+
+                const match_any = criteria.value.some(e => get_answer.answers.find(k => k.id == e) ? true : false);
+                skip_flag |= match_any;
+
+              } else if (criteria.operator == 'match_all') {
+
+                const match_all = criteria.value.every(e => get_answer.answers.find(k => k.id == e) ? true : false);
+                skip_flag |= match_all;
+
+              }
+            }
+          }
+
+          if (skip_flag) {
+            next_question_index = i;
+          }
+        }
+
+      } else {
+        next_question_index = i;
+      }
+
+      if (next_question_index >= 0) {
+        break;
+      }
+    }
+
+    if (next_question_index >= 0) {
+      update_state = {
+        ...update_state,
+        current_question_index: next_question_index,
+        answer: []
+      }
+
+      this.setState(update_state);
+    } else {
+      let answers = update_state.answers;
+      let total = 0;
+
+      this.setState(update_state);
+
+      for (let i = 0; i < answers.length; i++) {
+        let answer = answers[i];
+
+        for (let j = 0; j < answer.answers.length; j++) {
+          let a = answer.answers[j];
+          total += a.weight;
+        }
+      }
+
+      total = parseFloat(total).toFixed(2);
+
+      this._goTo('TestResultScreen', { total_weight: total });
+    }
+
   }
 
   _checkNextQuestion() {
@@ -188,7 +343,7 @@ export default class CreateYourStory extends React.Component {
       ...this.state,
       answers: [
         ...this.state.answers,
-        selected_answer
+        answer
       ]
     }, () => {
       let next_question_index = -1;
@@ -285,16 +440,19 @@ export default class CreateYourStory extends React.Component {
   }
 
   _renderNextButton() {
+    let hasSelectedAnswer = this.state.answer.length > 0;
     return (
       <View style={[Style.boxShadow, styles.nextButton]}>
         <TouchableOpacity
-          onPress={() => this._checkNextQuestion()}
-          style={styles.nextBtnAction}
+          onPress={() => { hasSelectedAnswer ? this._checkNextQuestionV2() : null }}
+          style={[styles.nextBtnAction, {
+            backgroundColor: hasSelectedAnswer ? Color.pink : Color.gray,
+          }]}
           activeOpacity={0.8}
         >
           <View style={{ width: 58 }} />
           <View style={styles.coverNextText}>
-            <Text style={styles.nextText}>Next</Text>
+            <Text style={[styles.nextText, { color: hasSelectedAnswer ? Color.white : Color.textBlack }]}>Next</Text>
           </View>
           <PlaySound
             fileName={'register'}
@@ -314,11 +472,12 @@ export default class CreateYourStory extends React.Component {
       <View style={{ flex: 1 }}>
         <StatusBar barStyle={'light-content'} backgroundColor={Color.pink} />
         {this._renderToolbar()}
+        {this._renderHeader()}
         <ScrollView
           style={{ flex: 1 }}
           contentContainerStyle={{ flexGrow: 1 }}
           showsVerticalScrollIndicator={false}>
-          {this._renderHeader()}
+          {/* {this._renderHeader()} */}
           <View style={[Style.container, { flex: 1, marginBottom: 0 }]}>
             {this._renderCards()}
           </View>
