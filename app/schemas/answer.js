826cf7e8f2ee55e89a2111b9dcc08f2039020aa7
Sovanda <sovanda.chhou@gmail.com>
2021-03-05 14:50:02 +0700

    questionnair schema realm, build release

---
+++ app/schemas/answer.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const AnswerSchema = {
+  name: 'Answer',
+  // primaryKey: 'id',
+  properties: {
+    id: 'int',
+    title: 'string',
+    weight: 'double'
+  }
+};
+
+
+
+export default AnswerSchema;
