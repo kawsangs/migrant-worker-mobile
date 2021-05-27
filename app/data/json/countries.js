ff45579a615d8bab71cfab5560b9b9957277235a
radin reth <radin.reth@gmail.com>
2021-05-27 11:30:44 +0700

    Update json data with required asset

--- app/data/json/countries.js
+++ app/data/json/countries.js
@@ -1,62 +1,62 @@
 export default [
-    {
-        "id": 13,
-        "name": "Cambodia",
-        "emoji_flag": "🇰🇭"
-    },
-    {
-        "id": 14,
-        "name": "Thailand",
-        "emoji_flag": "🇹🇭"
-    },
-    {
-        "id": 15,
-        "name": "Vietnam",
-        "emoji_flag": "🇻🇳"
-    },
-    {
-        "id": 16,
-        "name": "Laos",
-        "emoji_flag": "🇱🇦"
-    },
-    {
-        "id": 17,
-        "name": "Andorra",
-        "emoji_flag": "🇦🇩"
-    },
-    {
-        "id": 18,
-        "name": "Bangladesh",
-        "emoji_flag": "🇧🇩"
-    },
-    {
-        "id": 19,
-        "name": "Japan",
-        "emoji_flag": "🇯🇵"
-    },
-    {
-        "id": 20,
-        "name": "Norway",
-        "emoji_flag": "🇳🇴"
-    },
-    {
-        "id": 21,
-        "name": "Qatar",
-        "emoji_flag": "🇶🇦"
-    },
-    {
-        "id": 22,
-        "name": "Slovakia",
-        "emoji_flag": "🇸🇰"
-    },
-    {
-        "id": 23,
-        "name": "Antarctica",
-        "emoji_flag": "🇦🇶"
-    },
-    {
-        "id": 24,
-        "name": "Anguilla",
-        "emoji_flag": "🇦🇮"
-    }
+  {
+    "id": 13,
+    "name": "Cambodia",
+    "emoji_flag": "🇰🇭"
+  },
+  {
+    "id": 14,
+    "name": "Thailand",
+    "emoji_flag": "🇹🇭"
+  },
+  {
+    "id": 15,
+    "name": "Vietnam",
+    "emoji_flag": "🇻🇳"
+  },
+  {
+    "id": 16,
+    "name": "Laos",
+    "emoji_flag": "🇱🇦"
+  },
+  {
+    "id": 17,
+    "name": "Andorra",
+    "emoji_flag": "🇦🇩"
+  },
+  {
+    "id": 18,
+    "name": "Bangladesh",
+    "emoji_flag": "🇧🇩"
+  },
+  {
+    "id": 19,
+    "name": "Japan",
+    "emoji_flag": "🇯🇵"
+  },
+  {
+    "id": 20,
+    "name": "Norway",
+    "emoji_flag": "🇳🇴"
+  },
+  {
+    "id": 21,
+    "name": "Qatar",
+    "emoji_flag": "🇶🇦"
+  },
+  {
+    "id": 22,
+    "name": "Slovakia",
+    "emoji_flag": "🇸🇰"
+  },
+  {
+    "id": 23,
+    "name": "Antarctica",
+    "emoji_flag": "🇦🇶"
+  },
+  {
+    "id": 24,
+    "name": "Anguilla",
+    "emoji_flag": "🇦🇮"
+  }
 ]
--- app/data/json/institutions.js
+++ app/data/json/institutions.js
@@ -1,66 +1,69 @@
 export default [
-    {
-        "country_id": 13,
-        "institution": {
-            "id": 12,
-            "name": "អង្គការ​មនុស្ស​ចាស់​កម្ពុជា",
-            "kind": "ngo",
-            "address": "Battambang",
-            "logo_url": "1.jpeg",
-            "audio_url": null,
-            "contacts": [
-                {
-                    "id": 15,
-                    "type": "Phone",
-                    "value": "012 801 820"
-                }
-            ]
+  {
+    "country_id": 13,
+    "institution": {
+      "id": 12,
+      "name": "អង្គការមនុស្សចាស់កម្ពុជា",
+      "kind": "ngo",
+      "address": "Battambang",
+      "logo_url": "1.jpeg",
+      "logo": require('../../../app/assets/images/dummy/1.jpeg'),
+      "audio_url": null,
+      "contacts": [
+        {
+          "id": 15,
+          "type": "Phone",
+          "value": "012 801 820"
         }
-    },
-    {
-        "country_id": 13,
-        "institution": {
-            "id": 13,
-            "name": "អភិវឌ្ឍន៍សហគមន៍ជនបទនិងបរិស្ថាន ( អាសេដូ )",
-            "kind": "gov",
-            "address": "Phnom Penh",
-            "logo_url": "2.jpeg",
-            "audio_url": null,
-            "contacts": [
-                {
-                    "id": 16,
-                    "type": "Phone",
-                    "value": "054 710 446"
-                },
-                {
-                    "id": 17,
-                    "type": "Facebook",
-                    "value": "fb.com/test1"
-                },
-                {
-                    "id": 18,
-                    "type": "Whatsapp",
-                    "value": "054 710 446"
-                }
-            ]
-        }
-    },
-    {
-        "country_id": 13,
-        "institution": {
-            "id": 16,
-            "name": "testing with audio",
-            "kind": "other",
-            "address": "Kandal",
-            "logo_url": "3.jpeg",
-            "audio_url": "louvega_mambo.mp3",
-            "contacts": [
-                {
-                    "id": 22,
-                    "type": "Phone",
-                    "value": "32132132132"
-                }
-            ]
-        }
+      ]
     }
+  },
+  {
+    "country_id": 13,
+    "institution": {
+      "id": 13,
+      "name": "អភិវឌ្ឍន៍សហគមន៍ជនបទនិងបរិស្ថាន ( អាសេដូ )",
+      "kind": "gov",
+      "address": "Phnom Penh",
+      "logo_url": "2.jpeg",
+      "logo": require('../../../app/assets/images/dummy/2.jpeg'),
+      "audio_url": null,
+      "contacts": [
+        {
+          "id": 16,
+          "type": "Phone",
+          "value": "054 710 446"
+        },
+        {
+          "id": 17,
+          "type": "Facebook",
+          "value": "fb.com/test1"
+        },
+        {
+          "id": 18,
+          "type": "Whatsapp",
+          "value": "054 710 446"
+        }
+      ]
+    }
+  },
+  {
+    "country_id": 13,
+    "institution": {
+      "id": 16,
+      "name": "testing with audio",
+      "kind": "other",
+      "address": "Kandal",
+      "logo_url": "3.jpeg",
+      "logo": require('../../../app/assets/images/dummy/3.jpeg'),
+      "audio_url": "louvega_mambo.mp3",
+      "contacts": [
+        {
+          "id": 22,
+          "type": "Phone",
+          "value": "32132132132"
+        }
+      ]
+    }
+  }
 ]
