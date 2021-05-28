1d17855bc417a2041e9bd9c0e8f0724ac1c17b9f
Sokly <hengsokly23@gmail.com>
2021-05-28 10:00:59 +0700

    Video: change toolbar

--- app/navigators/app_navigator.js
+++ app/navigators/app_navigator.js
@@ -64,7 +64,13 @@ class AppNavigator extends Component {
       <>
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ title: "ចុះឈ្មោះ" }} />
-        <Stack.Screen name="ViewVideoScreen" component={ViewVideoScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ViewVideoScreen" component={ViewVideoScreen}
+          options={({route, navigation}) => ({
+            title: "វីដេអូ",
+            headerStyle: { backgroundColor: Color.primary },
+            headerRight: (props) => (<HomeButton navigation={navigation}/>),
+          })}
+        />
       </>
     )
   }
@@ -163,7 +169,13 @@ class AppNavigator extends Component {
             headerStyle: { backgroundColor: Color.primary },
           })}
         />
-        <Stack.Screen name="ViewVideoScreen" component={ViewVideoScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ViewVideoScreen" component={ViewVideoScreen}
+          options={({route, navigation}) => ({
+            title: "វីដេអូ",
+            headerStyle: { backgroundColor: Color.primary },
+            headerRight: (props) => (<HomeButton navigation={navigation}/>),
+          })}
+        />
         <Stack.Screen name="UserFormScreen" component={RegisterScreen} options={{ title: "កែតម្រូវគណនី" }} />
       </>
     )
