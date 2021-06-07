8e9a9bdf3905e462217b8d32c7c9eaeb68d7fab3
Sokly <hengsokly23@gmail.com>
2021-06-07 14:53:53 +0700

    Category and Form: re seed data for new version

--- app/db/schemas/schemaV1.js
+++ app/db/schemas/schemaV1.js
@@ -3,7 +3,8 @@ import helper from './helper';
 const schemaV0 = {
   schema: helper.getSchemas([]),
   schemaVersion: 1,
-  migration: (oldRealm, newRealm) => {},
+  // migration: (oldRealm, newRealm) => {},
+  deleteRealmIfMigrationNeeded: true,
 };
 
 export default schemaV0;
