b1e16d070b3c2fecad06a728ac60c9c2bcff24f8
Kimsanlim <kimsanlim27@gmail.com>
2021-06-29 11:39:38 +0700

    Add new library for playing YouTube video

--- app/screens/view_video.js
+++ app/screens/view_video.js
@@ -1,32 +1,24 @@
 import React, { useRef, useState } from 'react';
 import { View, Dimensions } from 'react-native';
+import YouTube from 'react-native-youtube';
 
-import YoutubePlayer from 'react-native-youtube-iframe';
-import LoadingIndicator from '../components/loading_indicator';
+import { environment } from '../config/environment';
 
 const ViewVideo = ({ route, navigation }) => {
   const { videoId } = route.params;
   const playerRef = useRef(null);
   const [playing, setPlaying] = useState(true);
-  const [loading, setLoading] = useState(true);
   const { width, height } = Dimensions.get('window');
 
   return (
     <View style={{ backgroundColor: '#000', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <YoutubePlayer
-        height={200}
-        width={width}
+      <YouTube
+        apiKey={environment.youtube_api_key}
+        style={{ height: 200, width: width }}
         ref={playerRef}
         play={playing}
         videoId={videoId}
-        onReady={() => setLoading(false)}
       />
-
-      {loading &&
-        <View style={{ position: 'absolute' }}>
-          <LoadingIndicator loading={true} />
-        </View>
-      }
     </View>
   );
 };
