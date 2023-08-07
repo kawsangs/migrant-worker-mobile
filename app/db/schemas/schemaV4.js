450cfbf733f2de92e3851c921457f9bc1c48eaff
Kimsanlim <kimsanlim27@gmail.com>
2023-08-07 17:46:51 +0700

    Add type to form schema

--- app/db/schemas/schemaV4.js
+++ app/db/schemas/schemaV4.js
@@ -1,9 +1,11 @@
 import Institution from '../migrations/v3/institution'
+import Form from '../migrations/v4/form'
 import Video from '../migrations/v4/video'
 import helper from './helper';
 
 const changedSchemas = [
   { label: 'Institution', data: Institution },
+  { label: 'Form', data: Form },
   { label: 'Video', data: Video }
 ];
 
@@ -11,6 +13,12 @@ const schemaV4 = {
   schema: helper.getSchemas(changedSchemas),
   schemaVersion: 4,
   migration: (oldRealm, newRealm) => {
+    const oldObjects = oldRealm.objects('Form');
+    const newObjects = newRealm.objects('Form');
+
+    for (let i = 0; i < oldObjects.length; i++) {
+      newObjects[i].type = !oldObjects[i].type ? 'your_story' : oldObjects[i].type;
+    }
   }
 }
 
