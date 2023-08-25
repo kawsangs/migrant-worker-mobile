445a569cea92b65558acea215f9cabb1e9f0c00b
Kimsanlim <kimsanlim27@gmail.com>
2023-08-25 14:22:50 +0700

    Include device_type, device_id, device_os, and app_version when sending registered_token API request

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://192.168.0.104:3000/api/v1',
+  apiUrl: 'http://192.168.0.104:3000/api/v2',
   domain: 'http://192.168.0.104:3000',
   state: 'development',
   access_token: 'e9f4be3f6a15e76741bd4b161a75fed3',
